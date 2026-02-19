986882a33dab913e511543c6e43b29ae54a9566c
keni <14bass@kk.ri>
2026-02-19 15:17:22 +0300

    added comment

---
+++ services/comment/api_comment.py
@@ -0,0 +1,23 @@
+from utils.helper import Helper
+import requests
+import allure
+
+from services.comment.comment_endpoints import CommentEndpoints
+
+
+class ApiComment(Helper):
+    def __init__(self, http_session: requests.Session, endpoints: CommentEndpoints, timeout: int = 15):
+        self.http_session = http_session
+        self.endpoint = endpoints
+        self.timeout = timeout
+
+    def _json(self, response: requests.Response):
+        try:
+            return response.json()
+        except ValueError:
+            return {"text": response.text}
+
+    def _check_status_code(self, response: requests.Response, ok_statuses: list[int]) -> dict:
+        body = self._json(response)
+        assert response.status_code in ok_statuses, body
+        return body
